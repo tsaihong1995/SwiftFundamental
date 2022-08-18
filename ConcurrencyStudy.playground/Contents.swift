8a24ac95dee99a20b7d1e0f6354d070f513cfe57
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-08-17 20:27:05 -0400

    Adding example of Main Queue and Global Concurrent Queues

--- ConcurrencyStudy.playground/Contents.swift
+++ ConcurrencyStudy.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// Difference between global concurrent queue and main queue.
+
+DispatchQueue.main.async {
+    print(Thread.isMainThread ? "Execution on Main Thread" : "Execution on other thread")
+}
+
+DispatchQueue.global().async {
+    print(Thread.isMainThread ? "Execution on Main Thread" : "Execution on Golbal concurrent queue")
+}
+
+/*
+  Output:
+ 1. Execution on Golbal concurrent queue
+ 2. Execution on Main Thread
+ 
+ Note:
+ 1. Main Queue:
+ - System Created
+ - Serial
+ - Uses Main Thread
+ - UI related operations must be performed on Main Queue
+ 
+ 2. Global Concurrent Queues
+ - System Created
+ - Concurrent
+ - Do not use Main Thread
+ */
